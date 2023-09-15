1b863c963df9c74384d78dfdd9b19704ecbed521
Tom Montgomery <montgomery3681@gmail.com>
2023-09-14 22:26:39 -0600

    Initial Commit

---
+++ Design_M1L1_Buttons/Design_M1L1_ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1L1_ButtonsApp.swift
+//  Design_M1L1_Buttons
+//
+//  Created by tom montgomery on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1L1_ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
